abbc8a278506d416fe06ddf4b5608eb27e9e90bc
Navabi-m <navabi.hossein@gmail.com>
2023-08-20 09:53:57 +0330

    add routing cinfiguration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home" , component: AppComponent},
+  {
+    path: "crawler",
+    loadChildren: () =>
+    import("./modules/crawler/crawler.module").then(
+      (m) => m.CrawlerModule
+    ),
+
+  },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/crawler/crawler-routing.module.ts
+++ src/app/modules/crawler/crawler-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchUrlComponent } from './components/search-url/search-url.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'search',
+    component: SearchUrlComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
