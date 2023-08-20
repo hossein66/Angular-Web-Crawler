66e65286f6cef997aebcd844609ee251ae2a67e9
Navabi-m <navabi.hossein@gmail.com>
2023-08-20 09:24:46 +0330

    create structure of project

---
+++ src/app/modules/crawler/crawler.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CrawlerRoutingModule } from './crawler-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CrawlerRoutingModule
+  ]
+})
+export class CrawlerModule { }
