c6542a7b106f68c7773720f56f90a8f78d68d7cf
Navabi-m <navabi.hossein@gmail.com>
2023-08-20 09:39:47 +0330

    add shared module

---
+++ src/app/modules/crawler/shared/shared.module.ts
@@ -0,0 +1,42 @@
+﻿import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from "./material.module";
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MaterialModule
+ ],
+  entryComponents: [
+    // All components about to be loaded "dynamically" need to be declared in the entryComponents section.
+  ],
+  declarations: [
+ ],
+  exports: [
+    // common and shared components/directives/pipes between more than one module and components will be listed here.
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MaterialModule
+
+  ],
+  /* No providers here! Since they’ll be already provided in AppModule. */
+})
+export class SharedModule {
+
+  static forRoot(): ModuleWithProviders<SharedModule> {
+    // Forcing the whole app to use the returned providers from the AppModule only.
+    return {
+      ngModule: SharedModule,
+      providers: [   
+ 
+     ],
+    };
+  }
+}
+
